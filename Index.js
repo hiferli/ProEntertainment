2085c7a84c4f8a59f2c8b70cc367b99d07e2b1cb
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2024-07-13 20:28:44 +0530

    Adding Introduction Section

---
+++ Index.js
@@ -0,0 +1,15 @@
+var typed = new Typed("#word", {
+    strings: [
+        "Entertainment!",
+        "Amusement!",
+        "Cinema!",
+        "Glamour!",
+        "Blockbusters!",
+        "Luxury!",
+        "Excitement!",
+        "Extravagance!",
+        "Thrill!",
+    ],
+    typeSpeed: 100,
+    loop: true,
+});
