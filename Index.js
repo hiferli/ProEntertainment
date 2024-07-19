6ba9346f3867336eb1448062741d8760c5da06e7
Mayank Sharma <mayanksharma18567@gmail.com>
2024-07-19 17:41:24 +0530

    Added review page and functionality to review form and newsLetter form

--- Index.js
+++ Index.js
@@ -13,3 +13,41 @@ var typed = new Typed("#word", {
     typeSpeed: 100,
     loop: true,
 });
+
+var newsLetter=(function(){
+console.log("Akanksha")
+    var prevNewsLettData=localStorage.getItem('newsLetterData');
+    var curNewsLettData=[]
+    
+
+    if(prevNewsLettData){
+        curNewsLettData=JSON.parse(prevNewsLettData);
+        console.log(curNewsLettData)
+    }
+
+    function storingNewsLetterData(){
+        const fname=document.getElementById('floatingFnameInput').value
+        const lname=document.getElementById('floatingLnameInput').value
+        const email=document.getElementById('floatingEmailInput').value
+        const password=document.getElementById('floatingPassword').value
+        const obj={fnmae:fname,
+                    lname:lname,
+                    email:email,
+                    password:password
+        }
+
+        curNewsLettData.push(obj);
+        localStorage.setItem('newsLetterData',JSON.stringify(curNewsLettData));
+        console.log(curNewsLettData)
+
+    }
+    
+    return storingNewsLetterData;
+})();
+
+function submitingNewsLetterForm(){     
+    console.log("akanksha was....")
+    newsLetter();
+    
+
+}
