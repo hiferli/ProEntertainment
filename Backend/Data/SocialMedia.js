79377a3abd1e1b58e69e9dd76cea3193a846a7a0
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2024-07-19 23:50:02 +0530

    Adding Social Media Page

--- Backend/Data/SocialMedia.js
+++ Backend/Data/SocialMedia.js
@@ -2,13 +2,13 @@ export const socialMediaData = {
     posts: [
         {
             platform: "Instagram",
-            photo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fin.pinterest.com%2Fpin%2Fig-afifahhzahrahh-movie-night--351912463988879%2F&psig=AOvVaw0MfUodf8pPnconpcFLTD02&ust=1721478155314000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCLCBgoGMs4cDFQAAAAAdAAAAABAI",
+            photo: "https://i.pinimg.com/736x/96/79/ae/9679aec71ebc34bb6f9586c35f0d1917.jpg",
             username: "user123",
             likes: 1024,
         },
         {
             platform: "Facebook",
-            photo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fin.pinterest.com%2Fpin%2Flife-in-december--44543483809218337%2F&psig=AOvVaw0MfUodf8pPnconpcFLTD02&ust=1721478155314000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCLCBgoGMs4cDFQAAAAAdAAAAABAQ",
+            photo: "https://img.freepik.com/premium-vector/instagram-story-template-with-movie-night-cinema-conceptwatercolor-style_75071-3744.jpg",
             username: "fb_user456",
             likes: 768,
         },
